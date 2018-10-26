83ef5ff4bd34a9a35e85df36c98837a9d8689fb5
luz <adrivioleta@hotmail.com>
2018-10-26 13:50:33 -0500

    Updates

---
+++ precache-manifest.97a2961bf710b35f02c417b14d268253.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "48ac19e69bbb5c76cd97844a23236457",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/media/hamburguesa.48ac19e6.png"
+  },
+  {
+    "revision": "db3000449ee3cfd6d8bb",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/js/runtime~main.db300044.js"
+  },
+  {
+    "revision": "76340e945e9ccdccd1b0",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/js/main.76340e94.chunk.js"
+  },
+  {
+    "revision": "1d85c4ba57f02a7115ca",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/js/1.1d85c4ba.chunk.js"
+  },
+  {
+    "revision": "76340e945e9ccdccd1b0",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/css/main.60ce56e0.chunk.css"
+  },
+  {
+    "revision": "1d85c4ba57f02a7115ca",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/css/1.4819ac25.chunk.css"
+  },
+  {
+    "revision": "8212187bcf1a9c8100b115c4bbcbedb1",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/index.html"
+  }
+];
